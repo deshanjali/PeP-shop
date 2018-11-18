ea01927567a20e010b922928f07e391c34b97500
Deshanjali <deshanjali1996824diyasena@gmail.com>
2018-11-18 07:31:41 +0530

    initial commit - setting up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAgfWDWkh8T2f_Xp_UjNVpVs6_4Dgr0L0o",
+    authDomain: "pep-shop-7f9f9.firebaseapp.com",
+    databaseURL: "https://pep-shop-7f9f9.firebaseio.com",
+    projectId: "pep-shop-7f9f9",
+    storageBucket: "pep-shop-7f9f9.appspot.com",
+    messagingSenderId: "535166075557"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAgfWDWkh8T2f_Xp_UjNVpVs6_4Dgr0L0o",
+    authDomain: "pep-shop-7f9f9.firebaseapp.com",
+    databaseURL: "https://pep-shop-7f9f9.firebaseio.com",
+    projectId: "pep-shop-7f9f9",
+    storageBucket: "pep-shop-7f9f9.appspot.com",
+    messagingSenderId: "535166075557"
+  }
 };
 
 /*
