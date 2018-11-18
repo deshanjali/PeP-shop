72d1a34c56d6225d7becbc0e400a99e33543a50b
Deshanjali <deshanjali1996824diyasena@gmail.com>
2018-11-18 09:35:58 +0530

    routing and extract navBar, Used bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,48 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import {  RouterModule } from '@angular/router';
+
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: '', component:HomeComponent },
+      { path: 'products', component:ProductsComponent },
+      { path: 'check-out', component:CheckOutComponent },
+      { path: 'shopping-cart', component:ShoppingCartComponent },
+      { path: 'order-success', component:OrderSuccessComponent },
+      { path: 'login', component:LoginComponent },
+      { path: 'admin/orders', component:AdminOrdersComponent },
+      { path: 'admin/products', component:AdminProductsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
