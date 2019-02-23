5f7afed19a139e65ed4d5cc42460df5514344200
Deshanjali <deshanjali1996824diyasena@gmail.com>
2019-02-23 21:00:05 +0530

    implementing the login with google

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAgfWDWkh8T2f_Xp_UjNVpVs6_4Dgr0L0o",
-    authDomain: "pep-shop-7f9f9.firebaseapp.com",
-    databaseURL: "https://pep-shop-7f9f9.firebaseio.com",
-    projectId: "pep-shop-7f9f9",
-    storageBucket: "pep-shop-7f9f9.appspot.com",
-    messagingSenderId: "535166075557"
+    apiKey: "AIzaSyDcFUkIdh11B_1j6UCuqzdayKARDeUNpzw",
+    authDomain: "pep-shop-7dadf.firebaseapp.com",
+    databaseURL: "https://pep-shop-7dadf.firebaseio.com",
+    projectId: "pep-shop-7dadf",
+    storageBucket: "pep-shop-7dadf.appspot.com",
+    messagingSenderId: "469858396864"
   }
 };
