c36395c27af77f87ba55c95e914984e750c1485f
Deshanjali <deshanjali1996824diyasena@gmail.com>
2019-03-04 06:50:28 +0530

    manage admin links

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,13 +3,19 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { switchMap } from 'rxjs/operators';
+import { UserService } from './user.service';
+import { of } from 'rxjs/';
 
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
@@ -23,4 +29,11 @@ export class AuthService {
   logout(){
     this.afAuth.auth.signOut();
   }
+
+  get appUser$() : Observable<AppUser> {
+    return this.user$.pipe(switchMap(user => {
+      if (user) return this.userService.get(user.uid);
+      return Observable.of(null);
+    }));
+    }
 }
